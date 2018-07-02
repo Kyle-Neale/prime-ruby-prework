a05ede628c4177325a928c8a31963a47ea3dfe66
Kyle Neale <kyle.a.neale@gmail.com>
2018-07-02 20:18:42 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 def prime?(number)
   if number < 2
-    return false
+    false
   end
   for num in (2..(number - 1))
     if number % num == 0
-      return false
+      false
     end 
   end
-  return true
+  true
 end
